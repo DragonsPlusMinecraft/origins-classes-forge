c45bf61d1492f2a52876e9ffdfaeec6d1ad1fc6c
LimonBlaze <1195384759@qq.com>
2022-04-16 13:42:51 +0800

    Improved NbtType

--- src/main/java/limonblaze/originsclasses/util/NbtType.java
+++ src/main/java/limonblaze/originsclasses/util/NbtType.java
@@ -1,23 +1,24 @@
 package limonblaze.originsclasses.util;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 
 import java.util.UUID;
 import java.util.function.BiFunction;
 
 public class NbtType<T> {
-    public static final NbtType<Byte> BYTE = new NbtType<>(1, CompoundTag::getByte);
-    public static final NbtType<Short> SHORT = new NbtType<>(2, CompoundTag::getShort);
-    public static final NbtType<Integer> INT = new NbtType<>(3, CompoundTag::getInt);
-    public static final NbtType<Long> LONG = new NbtType<>(4, CompoundTag::getLong);
-    public static final NbtType<Float> FLOAT = new NbtType<>(5, CompoundTag::getFloat);
-    public static final NbtType<Double> DOUBLE = new NbtType<>(6, CompoundTag::getDouble);
-    public static final NbtType<byte[]> BYTE_ARRAY = new NbtType<>(7, CompoundTag::getByteArray);
-    public static final NbtType<String> STRING = new NbtType<>(8, CompoundTag::getString);
+    public static final NbtType<Byte> BYTE = new NbtType<>(Tag.TAG_BYTE, CompoundTag::getByte);
+    public static final NbtType<Short> SHORT = new NbtType<>(Tag.TAG_SHORT, CompoundTag::getShort);
+    public static final NbtType<Integer> INT = new NbtType<>(Tag.TAG_INT, CompoundTag::getInt);
+    public static final NbtType<Long> LONG = new NbtType<>(Tag.TAG_LONG, CompoundTag::getLong);
+    public static final NbtType<Float> FLOAT = new NbtType<>(Tag.TAG_FLOAT, CompoundTag::getFloat);
+    public static final NbtType<Double> DOUBLE = new NbtType<>(Tag.TAG_DOUBLE, CompoundTag::getDouble);
+    public static final NbtType<byte[]> BYTE_ARRAY = new NbtType<>(Tag.TAG_BYTE_ARRAY, CompoundTag::getByteArray);
+    public static final NbtType<String> STRING = new NbtType<>(Tag.TAG_STRING, CompoundTag::getString);
     //TagList is not compatible with the getter bifunction
-    public static final NbtType<CompoundTag> COMPOUND = new NbtType<>(10, CompoundTag::getCompound);
-    public static final NbtType<int[]> INT_ARRAY = new NbtType<>(11, CompoundTag::getIntArray);
-    public static final NbtType<long[]> LONG_ARRAY = new NbtType<>(12, CompoundTag::getLongArray);
+    public static final NbtType<CompoundTag> COMPOUND = new NbtType<>(Tag.TAG_COMPOUND, CompoundTag::getCompound);
+    public static final NbtType<int[]> INT_ARRAY = new NbtType<>(Tag.TAG_INT_ARRAY, CompoundTag::getIntArray);
+    public static final NbtType<long[]> LONG_ARRAY = new NbtType<>(Tag.TAG_LONG_ARRAY, CompoundTag::getLongArray);
     //TAG_ANY_NUMMERIC has no getter
     public static final NbtType<UUID> UUID = new NbtType<>(100, CompoundTag::getUUID) {
         @Override
