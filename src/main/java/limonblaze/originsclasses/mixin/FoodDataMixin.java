b23c234e0558ec10731596c4ae27d8a27e94f248
LimonBlaze <1195384759@qq.com>
2022-04-18 20:29:12 +0800

    Bumped to Origins 1.4.1.0

--- src/main/java/limonblaze/originsclasses/mixin/FoodDataMixin.java
+++ src/main/java/limonblaze/originsclasses/mixin/FoodDataMixin.java
@@ -3,6 +3,7 @@ package limonblaze.originsclasses.mixin;
 import limonblaze.originsclasses.util.NbtType;
 import limonblaze.originsclasses.util.NbtUtils;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
@@ -18,11 +19,11 @@ public abstract class FoodDataMixin {
 
     @Shadow public abstract void eat(int food, float saturationModifier);
 
-    @Inject(method = "eat(Lnet/minecraft/world/item/Item;Lnet/minecraft/world/item/ItemStack;)V", at = @At("TAIL"))
-    private void originsClasses$handleFoodBonus(Item item, ItemStack stack, CallbackInfo ci) {
-        FoodProperties food = item.getFoodProperties();
+    @Inject(method = "eat(Lnet/minecraft/world/item/Item;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/LivingEntity;)V", at = @At("TAIL"), remap = false)
+    private void originsClasses$handleFoodBonus(Item pItem, ItemStack pStack, LivingEntity entity, CallbackInfo ci) {
+        FoodProperties food = pStack.getFoodProperties(entity);
         if(food != null) {
-            NbtUtils.getOriginsClassesData(stack, NbtUtils.FOOD_BONUS, NbtType.FLOAT).ifPresent(f ->
+            NbtUtils.getOriginsClassesData(pStack, NbtUtils.FOOD_BONUS, NbtType.FLOAT).ifPresent(f ->
                 this.eat(Mth.floor(food.getNutrition() * f), food.getSaturationModifier()));
         }
     }
